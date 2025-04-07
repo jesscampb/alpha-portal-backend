22ffbfc3bc91f2a5404988329a577759b5a27ac6
Jessica Campbell <jesscampb91@gmail.com>
2025-04-07 20:38:58 +0200

    Created UserEntity and UserAddressEntity

---
+++ Infrastructure/Data/Entities/UserEntity.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.Data.Entities;
+
+public class UserEntity : IdentityUser
+{
+    public string? ImageFileName { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public UserAddressEntity? Address { get; set; }
+}
