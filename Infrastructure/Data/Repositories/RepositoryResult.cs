91aec2b1294dbf9a22458afa1e9c1ef0bb38e479
Jessica Campbell <jesscampb91@gmail.com>
2025-04-08 15:36:51 +0200

    Created RepositoryResult for handling result statuses

---
+++ Infrastructure/Data/Repositories/RepositoryResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Infrastructure.Data.Repositories;
+
+public class RepositoryResult
+{
+    public bool Success { get; set; }
+    public int? StatusCode { get; set; }
+    public string? ErrorMessage { get; set; }
+}
+
+public class RepositoryResult<T> : RepositoryResult
+{
+    public T? Result { get; set; }
+}
