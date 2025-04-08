f5661dbd93188c69cded3887b9134e7231aacf89
Jessica Campbell <jesscampb91@gmail.com>
2025-04-08 11:14:19 +0200

    Base setup

---
+++ Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using Infrastructure.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Repositories;
+
+public abstract class BaseRepository<TEntity> where TEntity : class
+{
+    protected readonly AppDbContext _context;
+    protected readonly DbSet<TEntity> _table;
+
+    protected BaseRepository(AppDbContext context)
+    {
+        _context = context;
+        _table = context.Set<TEntity>();
+    }
+}
