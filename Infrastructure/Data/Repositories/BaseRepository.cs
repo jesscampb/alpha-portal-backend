287ef7f9d60d7fa76e3ecac357bcb22d9d713a0c
Jessica Campbell <jesscampb91@gmail.com>
2025-04-08 11:40:22 +0200

    Added support for caching and TModel for model convertion in BaseRepository

--- Infrastructure/Data/Repositories/BaseRepository.cs
+++ Infrastructure/Data/Repositories/BaseRepository.cs
@@ -1,16 +1,22 @@
 ﻿using Infrastructure.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Infrastructure.Data.Repositories;
 
-public abstract class BaseRepository<TEntity> where TEntity : class
+public abstract class BaseRepository<TEntity, TModel> where TEntity : class
 {
     protected readonly AppDbContext _context;
     protected readonly DbSet<TEntity> _table;
+    protected readonly IMemoryCache _cache;
+    protected readonly string _cacheKey;
 
-    protected BaseRepository(AppDbContext context)
+    protected BaseRepository(AppDbContext context, IMemoryCache cache)
     {
         _context = context;
+        _cache = cache;
+
         _table = context.Set<TEntity>();
+        _cacheKey = $"{typeof(TEntity).Name}_All";
     }
 }
