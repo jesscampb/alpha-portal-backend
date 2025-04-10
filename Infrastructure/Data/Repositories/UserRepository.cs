54f4fe89f49bb1261fbe168a5768a0c15c858950
Jessica Campbell <jesscampb91@gmail.com>
2025-04-10 22:41:21 +0200

    Created UserRepository in Repositories

---
+++ Infrastructure/Data/Repositories/UserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Data.Entities;
+
+namespace Infrastructure.Data.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<UserEntity>(context)
+{
+}
