2e062d7a2e93bb7d364da12d62cdd31b522f0512
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 17:01:50 +0200

    Created ClientRepository and interface

---
+++ Infrastructure/Data/Repositories/ClientRepository.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Data.Entities;
+
+namespace Infrastructure.Data.Repositories;
+
+public interface IClientRepository
+{
+}
+public class ClientRepository(AppDbContext context) : BaseRepository<ClientEntity>(context), IClientRepository
+{
+}
