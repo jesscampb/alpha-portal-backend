528d371dda5fcd55064ab599ad34ec457a03be63
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 18:01:32 +0200

    Registered ProjectRepository and interface in Program

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddScoped<IUserAddressRepository, UserAddressRepository>();
 builder.Services.AddScoped<IStatusRepository, StatusRepository>();
 builder.Services.AddScoped<IClientRepository, ClientRepository>();
 builder.Services.AddScoped<IClientBillingAddressRepository, ClientBillingAddressRepository>();
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 
 var app = builder.Build();
 
