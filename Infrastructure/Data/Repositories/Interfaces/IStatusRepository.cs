237c587de7fe9d65422531d443e47f5dd9f0b3f5
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 16:44:05 +0200

    Moved interface to Repositories/Interfaces folder

---
+++ Infrastructure/Data/Repositories/Interfaces/IStatusRepository.cs
@@ -0,0 +1,7 @@
+﻿using Infrastructure.Data.Entities;
+
+namespace Infrastructure.Data.Repositories.Interfaces;
+
+internal interface IStatusRepository : IBaseRepository<StatusEntity>
+{
+}
--- Infrastructure/Data/Repositories/StatusRepository.cs
+++ Infrastructure/Data/Repositories/StatusRepository.cs
@@ -3,10 +3,6 @@ using Infrastructure.Data.Entities;
 using Infrastructure.Data.Repositories.Interfaces;
 
 namespace Infrastructure.Data.Repositories;
-
-internal interface IStatusRepository : IBaseRepository<StatusEntity>
-{
-}
 public class StatusRepository(AppDbContext context) : BaseRepository<StatusEntity>(context), IStatusRepository
 {
 }
