01bb0db8bda268192e162f1186ae3d9efa0e05d9
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 17:41:40 +0200

    Created ClientModel and ClientBillingAddressModel

---
+++ Infrastructure/Models/ClientBillingAddressModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public class ClientBillingAddressModel
+{
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+    public string City { get; set; } = null!;
+}
