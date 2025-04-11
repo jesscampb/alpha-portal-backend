2ad2c11bd435b44bf030a2eac4e25bf973393baa
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 16:42:41 +0200

    Created StatusRepository and interface

---
+++ Infrastructure/Data/Repositories/StatusRepository.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Data.Entities;
+using Infrastructure.Data.Repositories.Interfaces;
+
+namespace Infrastructure.Data.Repositories;
+
+internal interface IStatusRepository : IBaseRepository<StatusEntity>
+{
+}
+public class StatusRepository(AppDbContext context) : BaseRepository<StatusEntity>(context), IStatusRepository
+{
+}
