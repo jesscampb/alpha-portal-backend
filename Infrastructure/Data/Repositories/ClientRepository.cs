bfcb0587474dfa8363457c9b015dd3861af73ce2
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 17:03:51 +0200

    Moved IClientRepository to Repositories/Interfaces

--- Infrastructure/Data/Repositories/ClientRepository.cs
+++ Infrastructure/Data/Repositories/ClientRepository.cs
@@ -1,11 +1,8 @@
 ﻿using Infrastructure.Data.Contexts;
 using Infrastructure.Data.Entities;
+using Infrastructure.Data.Repositories.Interfaces;
 
 namespace Infrastructure.Data.Repositories;
-
-public interface IClientRepository
-{
-}
 public class ClientRepository(AppDbContext context) : BaseRepository<ClientEntity>(context), IClientRepository
 {
 }
