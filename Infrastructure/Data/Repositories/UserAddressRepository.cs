b65fb873c4a979c42f219e07e48bf5dbabcddfdb
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 17:04:51 +0200

    Moved IUserAddressRepository to Repositories/Interfaces. Forgot to do it before.

--- Infrastructure/Data/Repositories/UserAddressRepository.cs
+++ Infrastructure/Data/Repositories/UserAddressRepository.cs
@@ -3,10 +3,6 @@ using Infrastructure.Data.Entities;
 using Infrastructure.Data.Repositories.Interfaces;
 
 namespace Infrastructure.Data.Repositories;
-
-public interface IUserAddressRepository : IBaseRepository<UserAddressEntity>
-{
-}
 public class UserAddressRepository(AppDbContext context) : BaseRepository<UserAddressEntity>(context), IUserAddressRepository
 {
 }
