533c7cb1c3bb60b3c7eeb9d014b65c0b5c86719e
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 17:46:21 +0200

    Created ProjectRepository and interface

---
+++ Infrastructure/Data/Repositories/ProjectRepository.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Data.Contexts;
+using Infrastructure.Data.Entities;
+
+namespace Infrastructure.Data.Repositories;
+
+public interface IProjectRepository
+{
+}
+public class ProjectRepository(AppDbContext context) : BaseRepository<ProjectEntity>(context), IProjectRepository
+{
+}
