ac99c4357f5f578cd76ece86424b7a9cab704df2
Jessica Campbell <jesscampb91@gmail.com>
2025-04-11 12:14:15 +0200

    Created UserModel and UserAddressModel

---
+++ Infrastructure/Models/UserAddressModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure.Models;
+
+public class  UserAddressModel
+{
+public string? StreetName { get; set; }
+public string? PostalCode { get; set; }
+public string? City { get; set; }
+}
---
+++ Infrastructure/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Infrastructure.Models;
+
+public class UserModel
+{
+    public string Id { get; set; } = null!;
+    public string? ImageFileName { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? JobTitle { get; set; }
+    public string? MemberRole { get; set; }
+    public UserAddressModel? Address { get; set; }
+}
