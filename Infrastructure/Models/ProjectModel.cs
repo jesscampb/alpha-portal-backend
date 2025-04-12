062fd463be0bc3276eab11cddefe41847be19088
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 14:49:18 +0200

    Updated ProjectModel props

--- Infrastructure/Models/ProjectModel.cs
+++ Infrastructure/Models/ProjectModel.cs
@@ -5,11 +5,12 @@ public class ProjectModel
     public string Id { get; set; } = null!;
     public string? ImageFileName { get; set; }
     public string ProjectName { get; set; } = null!;
-    public ClientModel Client { get; set; } = null!;
     public string? Description { get; set; }
     public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public decimal Budget { get; set; }
+    public DateTime? EndDate { get; set; }
+    public DateTime Created { get; set; }
+    public decimal? Budget { get; set; }
+    public ClientModel Client { get; set; } = null!;
     public UserModel User { get; set; } = null!;
     public ProjectStatusModel Status { get; set; } = null!;
 }
