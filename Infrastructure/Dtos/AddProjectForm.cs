e444d4d2643e74612ae9db6a95013f36c09d53fe
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 15:54:20 +0200

    Created AddProjectForm dto

---
+++ Infrastructure/Dtos/AddProjectForm.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class AddProjectForm
+{
+    public IFormFile? ImageFile { get; set; }
+    [Required]
+    public string ProjectName { get; set; } = null!;
+    [Required]
+    public string ClientId { get; set; } = null!;
+    public string? Description { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    [Required]
+    public string UserId { get; set; } = null!;
+    public decimal? Budget { get; set; }
+}
