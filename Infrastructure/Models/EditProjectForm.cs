7ecb97dc9e039072975bf875dab84341ca8a481e
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 16:02:28 +0200

    Created EditProjectForm

---
+++ Infrastructure/Models/EditProjectForm.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Models;
+
+public class EditProjectForm
+{
+    [Required]
+    public string Id { get; set; } = null!;
+    public string? ImageFileName { get; set; }
+    public IFormFile? NewImageFile { get; set; }
+    [Required]
+    public string ProjectName { get; set; } = null!;
+    [Required]
+    public int ClientId { get; set; }
+    public string? Description { get; set; }
+    [Required]
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    [Required]
+    public string UserId { get; set; } = null!;
+    public decimal? Budget { get; set; }
+    [Required]
+    public int ProjectStatusId { get; set; }
+}
