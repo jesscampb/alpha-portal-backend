29082a5d04fe07882a9d266049530fc32ea89d50
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 15:27:14 +0200

    Created SignUpForm dto

---
+++ Infrastructure/Dtos/SignUpForm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class SignUpForm
+{
+    [Required]
+    public string FirstName { get; set; } = null!;
+    [Required]
+    public string LastName { get; set; } = null!;
+    [Required]
+    public string Email { get; set; } = null!;
+    [Required]
+    public string Password { get; set; } = null!;
+    [Required]
+    public bool IsAgreedToTerms { get; set; }
+}
