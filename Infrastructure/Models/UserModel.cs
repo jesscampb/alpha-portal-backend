269e06dd16d21aa9832dfe6e847d7b7ece130e67
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 14:35:19 +0200

    Updated UserModel props

--- Infrastructure/Models/UserModel.cs
+++ Infrastructure/Models/UserModel.cs
@@ -6,6 +6,8 @@ public class UserModel
     public string? ImageFileName { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
     public string? JobTitle { get; set; }
     public string? MemberRole { get; set; }
     public UserAddressModel? Address { get; set; }
