a058dab37abecf0b07c4662f5f63c10b1f8baa31
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 14:36:13 +0200

    Updated UserAddressModel props

--- Infrastructure/Models/UserAddressModel.cs
+++ Infrastructure/Models/UserAddressModel.cs
@@ -2,7 +2,7 @@
 
 public class  UserAddressModel
 {
-    public string? StreetName { get; set; }
-    public string? PostalCode { get; set; }
-    public string? City { get; set; }
+    public string StreetName { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
 }
