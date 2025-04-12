22289be7c0b95fcbdf60be4a2256bc2d698a56bd
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 14:54:29 +0200

    Updated ClientAddressModel props

--- Infrastructure/Models/ClientAddressModel.cs
+++ Infrastructure/Models/ClientAddressModel.cs
@@ -2,7 +2,7 @@
 
 public class ClientAddressModel
 {
-    public string? Street { get; set; }
-    public string? PostalCode { get; set; }
+    public string StreetName { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
     public string City { get; set; } = null!;
 }
