a9e6866d01495770a1b3323d987c71ea69efe3ff
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 00:09:45 +0200

    Added data annotations to AddClientForm

--- Infrastructure/Dtos/AddClientForm.cs
+++ Infrastructure/Dtos/AddClientForm.cs
@@ -7,15 +7,22 @@ public class AddClientForm
 {
     public IFormFile? ImageFile { get; set; }
     [Required]
+    [MinLength(1)]
     public string ClientName { get; set; } = null!;
     [Required]
+    [EmailAddress]
     public string Email { get; set; } = null!;
+    [RegularExpression(@"^\+?[0-9]{8,15}$")]
     public string? PhoneNumber { get; set; }
     [Required]
+    [MinLength(2)]
     public string StreetName { get; set; } = null!;
     [Required]
+    [StringLength(5, MinimumLength = 5)]
     public string PostalCode { get; set; } = null!;
     [Required]
+    [MinLength(2)]
     public string City { get; set; } = null!;
+    [MinLength(2)]
     public string? Reference { get; set; }
 }
