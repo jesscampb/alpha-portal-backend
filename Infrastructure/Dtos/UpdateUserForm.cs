6f21ba948cdd0e0e801af22f000f04b462591ec8
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 18:07:42 +0200

    Created UpdateUserForm dto

---
+++ Infrastructure/Dtos/UpdateUserForm.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class UpdateUserForm
+{
+    [Required]
+    public string Id { get; set; } = null!;
+    public string? ImageFileName { get; set; }
+    public IFormFile? NewImageFile { get; set; }
+    [Required]
+    public string FirstName { get; set; } = null!;
+    [Required]
+    public string LastName { get; set; } = null!;
+    [Required]
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+    public string? JobTitle { get; set; }
+    public string? Role { get; set; }
+    public string? StreetName { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
+}
