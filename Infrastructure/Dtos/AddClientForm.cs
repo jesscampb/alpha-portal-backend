4e01f493243cf031f553009b44293712ce55b1ce
Jessica Campbell <jesscampb91@gmail.com>
2025-04-12 18:21:23 +0200

    Created AddClientForm dto

---
+++ Infrastructure/Dtos/AddClientForm.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Dtos;
+
+public class AddClientForm
+{
+    public IFormFile? ImageFile { get; set; }
+    [Required]
+    public string ClientName { get; set; } = null!;
+    [Required]
+    public string Email { get; set; } = null!;
+    public string? PhoneNumber { get; set; }
+    [Required]
+    public string StreetName { get; set; } = null!;
+    [Required]
+    public string PostalCode { get; set; } = null!;
+    [Required]
+    public string City { get; set; } = null!;
+    public string? Reference { get; set; }
+}
