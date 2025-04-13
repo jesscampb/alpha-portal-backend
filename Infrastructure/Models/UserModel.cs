07997100def58330887ff3835e4b030248aadcbf
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 15:41:58 +0200

    Updated UserModel.

--- Infrastructure/Models/UserModel.cs
+++ Infrastructure/Models/UserModel.cs
@@ -1,14 +1,18 @@
-﻿namespace Infrastructure.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Models;
 
 public class UserModel
 {
     public string Id { get; set; } = null!;
     public string? ImageFileName { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
     public string? JobTitle { get; set; }
     public string? Role { get; set; }
-    public UserAddressModel? Address { get; set; }
+    public string? StreetName { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
 }
