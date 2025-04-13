04911561182db55accd296d8eeeebd4a17fcb2d9
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 16:05:50 +0200

    Update to better reflect database requirements

--- Infrastructure/Models/UserModel.cs
+++ Infrastructure/Models/UserModel.cs
@@ -1,14 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Infrastructure.Models;
+﻿namespace Infrastructure.Models;
 
 public class UserModel
 {
     public string Id { get; set; } = null!;
     public string? ImageFileName { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
-    public string? Email { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string? PhoneNumber { get; set; }
     public string? JobTitle { get; set; }
     public string? Role { get; set; }
