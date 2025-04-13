ed756324ea3b6a7f73905d0978518be89097e60d
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 17:14:07 +0200

    Created ProjectService

---
+++ Infrastructure/Services/ProjectService.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Data.Repositories.Interfaces;
+using Infrastructure.Dtos;
+using Infrastructure.Models;
+
+namespace Infrastructure.Services;
+
+public class ProjectService(IProjectRepository projectRepository)
+{
+    private readonly IProjectRepository _projectRepository = projectRepository;
+
+    public async Task<ProjectModel> CreateProjectAsync(AddProjectForm formData)
+    {
+
+    }
+}
+
+// Create (add project form)
+// Read (by id and all projects)
+// Update (update project form)
+// Delete
