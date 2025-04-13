e09813a995838891244f9d4ca9f18f27b51668b9
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 15:50:44 +0200

    Updated ClientModel

--- Infrastructure/Models/ClientModel.cs
+++ Infrastructure/Models/ClientModel.cs
@@ -4,10 +4,12 @@ public class ClientModel
 {
     public string Id { get; set; } = null!;
     public string? ImageFileName { get; set; }
-    public string ClientName { get; set; } = null!;
-    public string Email { get; set; } = null!;
+    public string? ClientName { get; set; }
+    public string? Email { get; set; }
     public string? PhoneNumber { get; set; }
-    public ClientAddressModel Address { get; set; } = null!;
+    public string? StreetName { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
     public string? Reference { get; set; }
     public bool IsActive { get; set; } = false;
 }
