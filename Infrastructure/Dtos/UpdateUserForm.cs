58eac4e4eeb7b7ac6e65fc8a15307fbb540216bc
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 13:07:54 +0200

    Added data annotations to UpdateUserForm

--- Infrastructure/Dtos/UpdateUserForm.cs
+++ Infrastructure/Dtos/UpdateUserForm.cs
@@ -9,16 +9,34 @@ public class UpdateUserForm
     public string Id { get; set; } = null!;
     public string? ExistingImageFileName { get; set; }
     public IFormFile? NewImageFile { get; set; }
+
     [Required]
+    [StringLength(50, MinimumLength = 2)]
     public string FirstName { get; set; } = null!;
+
     [Required]
+    [StringLength(50, MinimumLength = 2)]
     public string LastName { get; set; } = null!;
+
     [Required]
+    [EmailAddress]
+    [StringLength(100)]
     public string Email { get; set; } = null!;
+
+    [RegularExpression(@"^\+46\d{7,11}$")]
     public string? PhoneNumber { get; set; }
+
+    [StringLength(100)]
     public string? JobTitle { get; set; }
+
     public string? Role { get; set; }
+
+    [StringLength(100, MinimumLength = 2)]
     public string? StreetName { get; set; }
+
+    [RegularExpression(@"^\d{5}$")]
     public string? PostalCode { get; set; }
+
+    [StringLength(50, MinimumLength = 2)]
     public string? City { get; set; }
 }
