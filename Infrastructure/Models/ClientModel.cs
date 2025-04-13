94ea1b6579181ed4801c9b1a67e2f67329d41ebc
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 15:58:39 +0200

    Update to better reflect database requirements

--- Infrastructure/Models/ClientModel.cs
+++ Infrastructure/Models/ClientModel.cs
@@ -4,12 +4,12 @@ public class ClientModel
 {
     public string Id { get; set; } = null!;
     public string? ImageFileName { get; set; }
-    public string? ClientName { get; set; }
-    public string? Email { get; set; }
+    public string ClientName { get; set; } = null!;
+    public string Email { get; set; } = null!;
     public string? PhoneNumber { get; set; }
-    public string? StreetName { get; set; }
-    public string? PostalCode { get; set; }
-    public string? City { get; set; }
+    public string StreetName { get; set; } = null!;
+    public string PostalCode { get; set; } = null!;
+    public string City { get; set; } = null!;
     public string? Reference { get; set; }
     public bool IsActive { get; set; } = false;
 }
