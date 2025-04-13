e67dfe9ddeb8f097bc9c6049acb14550785f8a83
Jessica Campbell <jesscampb91@gmail.com>
2025-04-13 13:05:42 +0200

    Added data annotations to UpdateProjectForm

--- Infrastructure/Dtos/UpdateProjectForm.cs
+++ Infrastructure/Dtos/UpdateProjectForm.cs
@@ -7,21 +7,33 @@ public class UpdateProjectForm
 {
     [Required]
     public string Id { get; set; } = null!;
+
     public string? ExistingImageFileName { get; set; }
     public IFormFile? NewImageFile { get; set; }
+
     [Required]
+    [StringLength(100, MinimumLength = 2)]
     public string ProjectName { get; set; } = null!;
+
     [Required]
     public int ClientId { get; set; }
+
+    [StringLength(1000)]
     public string? Description { get; set; }
+
     [Required]
     [DataType(DataType.Date)]
     public DateTime StartDate { get; set; }
+
     [DataType(DataType.Date)]
     public DateTime? EndDate { get; set; }
+
     [Required]
     public string UserId { get; set; } = null!;
+
+    [Range(0, 10_000_000)]
     public decimal? Budget { get; set; }
+
     [Required]
     public int ProjectStatusId { get; set; }
 }
