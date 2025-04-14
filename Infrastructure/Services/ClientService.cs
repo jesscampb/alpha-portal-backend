d6e068f40a44fbd5e09dad19afbe4856113f1f26
Jessica Campbell <jesscampb91@gmail.com>
2025-04-14 13:15:25 +0200

    Create Client service done

---
+++ Infrastructure/Services/ClientService.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Data.Repositories;
+using Infrastructure.Data.Repositories.Interfaces;
+using Infrastructure.Dtos;
+using Infrastructure.Factories;
+using Infrastructure.Models;
+using System.Diagnostics;
+
+namespace Infrastructure.Services;
+
+public class ClientService(IClientRepository clientRepository)
+{
+    private readonly IClientRepository _clientRepository = clientRepository;
+
+    public async Task<ClientModel?> CreateProjectAsync(AddClientForm formData)
+    {
+        if (formData == null) return null;
+
+        try
+        {
+            var entity = ClientFactory.ToEntity(formData);
+            await _clientRepository.AddAsync(entity);
+
+            var model = ClientFactory.ToModel(entity);
+            return model;
+
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            return null;
+        }
+    }
+}
+
+// C
+// R1 - RA
+// U
+// D
