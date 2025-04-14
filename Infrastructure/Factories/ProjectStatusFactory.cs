7715205a4ac05e8c00922fb78e4f7e96006b6014
Jessica Campbell <jesscampb91@gmail.com>
2025-04-14 13:39:03 +0200

    Created ToModel(entity)

---
+++ Infrastructure/Factories/ProjectStatusFactory.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.Entities;
+using Infrastructure.Models;
+
+namespace Infrastructure.Factories;
+
+public class ProjectStatusFactory
+{
+    public static ProjectStatusModel ToModel(ProjectStatusEntity entity)
+    {
+        if (entity == null) return null!;
+
+        return new ProjectStatusModel
+        {
+            Id = entity.Id,
+            StatusName = entity.StatusName
+        };
+    }
+}
