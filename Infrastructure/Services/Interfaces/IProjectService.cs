7bfd9ce465d644577e6354d6d8b7f92472ad8576
Jessica Campbell <jesscampb91@gmail.com>
2025-04-14 12:41:42 +0200

    Created ProjectService interface. Registered in Program.

---
+++ Infrastructure/Services/Interfaces/IProjectService.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.Dtos;
+using Infrastructure.Models;
+
+namespace Infrastructure.Services.Interfaces
+{
+    public interface IProjectService
+    {
+        Task<ProjectModel?> CreateProjectAsync(AddProjectForm formData);
+        Task<bool> DeleteProjectAsync(string id);
+        Task<IEnumerable<ProjectModel>> GetAllProjectsAsync();
+        Task<ProjectModel?> GetProjectByIdAsync(string id);
+        Task<ProjectModel?> UpdateProjectAsync(UpdateProjectForm formData);
+    }
+}
