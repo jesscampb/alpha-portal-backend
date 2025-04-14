829ce7e54b9d4ab4d876daf3c123f2dd8e6b5bad
Jessica Campbell <jesscampb91@gmail.com>
2025-04-14 14:37:20 +0200

    Create ToEntity(AddUserForm)

---
+++ Infrastructure/Factories/UserFactory.cs
@@ -0,0 +1,30 @@
+﻿using Infrastructure.Data.Entities;
+using Infrastructure.Dtos;
+
+namespace Infrastructure.Factories
+{
+    public class UserFactory
+    {
+        public static UserEntity ToEntity(AddUserForm formData, string? newImageFileName = null)
+        {
+            if (formData == null) return null!;
+
+            return new UserEntity
+            {
+                ImageFileName = newImageFileName,
+                FirstName = formData.FirstName,
+                LastName = formData.LastName,
+                Email = formData.Email,
+                PhoneNumber = formData.PhoneNumber,
+                JobTitle = formData.JobTitle,
+                UserName = formData.Email,
+                Address = new UserAddressEntity
+                {
+                    StreetName = formData.StreetName ?? null!,
+                    PostalCode = formData.PostalCode ?? null!,
+                    City = formData.City ?? null!
+                }
+            };
+        }
+    }
+}
