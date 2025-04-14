d526bdfebd16faee01bb8b5975586ef3bebcbcde
Jessica Campbell <jesscampb91@gmail.com>
2025-04-14 10:46:59 +0200

    Removed OperationResult returns and try-catches (will move to services)

--- Infrastructure/Data/Repositories/BaseRepository.cs
+++ Infrastructure/Data/Repositories/BaseRepository.cs
@@ -1,8 +1,6 @@
-﻿using Infrastructure.Common;
-using Infrastructure.Data.Contexts;
+﻿using Infrastructure.Data.Contexts;
 using Infrastructure.Data.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System.Diagnostics;
 using System.Linq.Expressions;
 
 namespace Infrastructure.Data.Repositories;
@@ -18,131 +16,69 @@ public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where T
         _table = context.Set<TEntity>();
     }
 
-    public virtual async Task<OperationResult> ExistsAsync(Expression<Func<TEntity, bool>> filterByExpression)
+    public virtual async Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> filterByExpression)
     {
-        try
-        {
-            var result = await _table.AnyAsync(filterByExpression);
-            return result
-                ? new OperationResult { Succeeded = true, StatusCode = 200 }
-                : new OperationResult { Succeeded = false, StatusCode = 404 };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-            return new OperationResult { Succeeded = false, StatusCode = 500, ErrorMessage = ex.Message };
-        }
+        var result = await _table.AnyAsync(filterByExpression);
+        return result;
     }
 
-    public virtual async Task<OperationResult> AddAsync(TEntity entity)
+    public virtual async Task<bool> AddAsync(TEntity entity)
     {
-        if (entity == null)
-            return new OperationResult { Succeeded = false, StatusCode = 400 };
+        if (entity == null) return false;
 
-        try
-        {
-            _table.Add(entity);
-            await _context.SaveChangesAsync();
-
-            return new OperationResult { Succeeded = true, StatusCode = 201 };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-
-            return new OperationResult { Succeeded = false, StatusCode = 500, ErrorMessage = ex.Message };
-        }
+        _table.Add(entity);
+        await _context.SaveChangesAsync();
+        return true;
     }
 
-    public virtual async Task<OperationResult> UpdateAsync(TEntity entity)
+    public virtual async Task<bool> UpdateAsync(TEntity entity)
     {
-        if (entity == null)
-            return new OperationResult { Succeeded = false, StatusCode = 400 };
+        if (entity == null) return false;
 
-        try
-        {
-            _table.Update(entity);
-            await _context.SaveChangesAsync();
-
-            return new OperationResult { Succeeded = true, StatusCode = 200 };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-
-            return new OperationResult { Succeeded = false, StatusCode = 500, ErrorMessage = ex.Message };
-        }
+        _table.Update(entity);
+        await _context.SaveChangesAsync();
+        return true;
     }
 
-    public virtual async Task<OperationResult> DeleteAsync(TEntity entity)
+    public virtual async Task<bool> DeleteAsync(TEntity entity)
     {
-        if (entity == null)
-            return new OperationResult { Succeeded = false, StatusCode = 400 };
+        if (entity == null) return false;
 
-        try
-        {
-            _table.Remove(entity);
-            await _context.SaveChangesAsync();
-
-            return new OperationResult { Succeeded = true, StatusCode = 200 };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-
-            return new OperationResult { StatusCode = 500, Succeeded = false, ErrorMessage = ex.Message };
-        }
+        _table.Remove(entity);
+        await _context.SaveChangesAsync();
+        return true;
     }
 
-    public virtual async Task<OperationResult<TEntity>> GetAsync(Expression<Func<TEntity, bool>> filterByExpression, params Expression<Func<TEntity, object>>[] includes)
+    public virtual async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> filterByExpression, params Expression<Func<TEntity, object>>[] includes)
     {
-        try
-        {
-            IQueryable<TEntity> query = _table;
 
-            if (includes != null && includes.Length > 0)
-                foreach (var include in includes)
-                    query = query.Include(include);
+        IQueryable<TEntity> query = _table;
 
-            var entity = await query.FirstOrDefaultAsync(filterByExpression);
-            if (entity == null)
-                return new OperationResult<TEntity> { Succeeded = false, StatusCode = 404 };
+        if (includes != null && includes.Length > 0)
+            foreach (var include in includes)
+                query = query.Include(include);
 
-            return new OperationResult<TEntity> { Succeeded = true, StatusCode = 200, Result = entity };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-            return new OperationResult<TEntity> { Succeeded = false, StatusCode = 500, ErrorMessage = ex.Message };
-        }
+        var entity = await query.FirstOrDefaultAsync(filterByExpression);
+        return entity;
     }
 
-    public virtual async Task<OperationResult<IEnumerable<TEntity>>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortByExpression = null,
+    public virtual async Task<IEnumerable<TEntity>> GetAllAsync(bool orderByDescending = false, Expression<Func<TEntity, object>>? sortByExpression = null,
         Expression<Func<TEntity, bool>>? filterByExpression = null, params Expression<Func<TEntity, object>>[] includes)
     {
-        try
-        {
-            IQueryable<TEntity> query = _table;
+        IQueryable<TEntity> query = _table;
 
-            if (filterByExpression != null)
-                query = query.Where(filterByExpression);
+        if (filterByExpression != null)
+            query = query.Where(filterByExpression);
 
-            if (includes != null && includes.Length > 0)
-                foreach (var include in includes)
-                    query = query.Include(include);
+        if (includes != null && includes.Length > 0)
+            foreach (var include in includes)
+                query = query.Include(include);
 
-            if (sortByExpression != null)
-                query = orderByDescending ? query.OrderByDescending(sortByExpression) : query.OrderBy(sortByExpression);
+        if (sortByExpression != null)
+            query = orderByDescending ? query.OrderByDescending(sortByExpression) : query.OrderBy(sortByExpression);
 
-            var entities = await query.ToListAsync();
-            return new OperationResult<IEnumerable<TEntity>> { Succeeded = true, StatusCode = 200, Result = entities };
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine(ex.Message);
-
-            return new OperationResult<IEnumerable<TEntity>> { Succeeded = false, StatusCode = 500, ErrorMessage = ex.Message };
-        }
+        var entities = await query.ToListAsync();
+        return entities;
     }
 
 }
