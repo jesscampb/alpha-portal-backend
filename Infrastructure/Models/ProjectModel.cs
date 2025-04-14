522b3542f7442bf6a126279f48cee982c632b2da
Jessica Campbell <jesscampb91@gmail.com>
2025-04-14 17:22:54 +0200

    Update ProjectModel

--- Infrastructure/Models/ProjectModel.cs
+++ Infrastructure/Models/ProjectModel.cs
@@ -7,7 +7,7 @@ public class ProjectModel
     public string ProjectName { get; set; } = null!;
     public string? Description { get; set; }
     public DateTime StartDate { get; set; }
-    public DateTime? EndDate { get; set; }
+    public DateTime EndDate { get; set; }
     public DateTime Created { get; set; }
     public decimal? Budget { get; set; }
     public ClientModel Client { get; set; } = null!;
