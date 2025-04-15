52b62474933f2ad62088ba9e49207e63e561888b
Jessica Campbell <jesscampb91@gmail.com>
2025-04-15 15:03:09 +0200

    Create done

--- Presentation/Controllers/ProjectController.cs
+++ Presentation/Controllers/ProjectController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Infrastructure.Dtos;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Presentation.Controllers
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProjectController(IProjectService projectService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProjectController : ControllerBase
+    private readonly IProjectService _projectService = projectService;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(AddProjectForm formData)
     {
+        if (!ModelState.IsValid)
+            return BadRequest(formData);
+
+        var result = await _projectService.CreateProjectAsync(formData);
+        
+        if (result == null)
+            return BadRequest("Failed to create project");
+
+        return Ok(result);
     }
 }
