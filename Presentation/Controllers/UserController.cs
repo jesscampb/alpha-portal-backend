1cbc4ce2d8978d600e92331aa8339a316ec263dd
Jessica Campbell <jesscampb91@gmail.com>
2025-04-15 16:50:34 +0200

    Created UserController

---
+++ Presentation/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IUserService userService) : ControllerBase
+{
+    private readonly IUserService _userService = userService;
+}
