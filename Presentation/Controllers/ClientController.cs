2e34d2b65f60c15003e1005544e355e989d7d1b9
Jessica Campbell <jesscampb91@gmail.com>
2025-04-15 16:30:57 +0200

    Create done

--- Presentation/Controllers/ClientController.cs
+++ Presentation/Controllers/ClientController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Infrastructure.Dtos;
+using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class ClientController : ControllerBase
+public class ClientController(IClientService clientService) : ControllerBase
 {
+    private readonly IClientService _clientService = clientService;
+
+    [HttpPost]
+    public async Task<IActionResult> Create(AddClientForm formData)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(formData);
+
+        var result = await _clientService.CreateClientAsync(formData);
+
+        return result == null ? BadRequest() : Ok();
+    }
 }
