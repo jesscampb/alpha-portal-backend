261e3ecb334c62efc79e3315e817feb64212838e
Jessica Campbell <jesscampb91@gmail.com>
2025-04-15 16:07:42 +0200

    Created ProjectStatusController

---
+++ Presentation/Controllers/ProjectStatusController.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectStatusController(IProjectService projectService) : ControllerBase
+    {
+        private readonly IProjectService _projectService = projectService;
+
+
+    }
+}
