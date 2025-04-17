0d040d19d8c27bae081e76bbf5b2cb35d95144b7
Jessica Campbell <jesscampb91@gmail.com>
2025-04-17 11:39:35 +0200

    Create middleware and add constructor

---
+++ Presentation/Extensions/Middlewares/DefaultApiKeyMiddleware.cs
@@ -0,0 +1,16 @@
+﻿namespace Presentation.Extensions.Middlewares;
+
+public class DefaultApiKeyMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly IConfiguration _configuration;
+    private const string APIKEY_HEADER_NAME = "X-API-KEY";
+    private readonly string _apiKey = String.Empty;
+
+    public DefaultApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+    {
+        _next = next;
+        _configuration = configuration;
+        _apiKey = _configuration["ApiKeys:Default"]!;
+    }
+}
