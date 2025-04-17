b6412349457651b5de2e639dc55d3fc45d4d9955
Jessica Campbell <jesscampb91@gmail.com>
2025-04-17 12:40:10 +0200

    Added logic and usage

---
+++ Presentation/Extensions/Attributes/AdminApiKeyAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Presentation.Extensions.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class AdminApiKeyAttribute : Attribute, IAsyncActionFilter
+{
+    public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+    {
+        var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+        var adminApiKey = configuration["ApiKeys:Admin"];
+
+        if (!context.HttpContext.Request.Headers.TryGetValue("X-ADM-API-KEY", out var providedApiKey))
+        {
+            context.Result = new UnauthorizedObjectResult("Invalid API key or API key is missing.");
+            return;
+        }
+
+        if (string.IsNullOrEmpty(adminApiKey) || !string.Equals(providedApiKey, adminApiKey))
+        {
+            context.Result = new UnauthorizedObjectResult("Invalid API key.");
+            return;
+        }
+
+        await next();
+    }
+}
