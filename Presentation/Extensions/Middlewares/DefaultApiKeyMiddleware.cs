5ed136e1af8a3708a19c40205055e4074e3819da
Jessica Campbell <jesscampb91@gmail.com>
2025-04-17 12:06:42 +0200

    Created InvokeAsync method

--- Presentation/Extensions/Middlewares/DefaultApiKeyMiddleware.cs
+++ Presentation/Extensions/Middlewares/DefaultApiKeyMiddleware.cs
@@ -1,16 +1,31 @@
-﻿namespace Presentation.Extensions.Middlewares;
+﻿using Microsoft.Identity.Client;
 
-public class DefaultApiKeyMiddleware
+namespace Presentation.Extensions.Middlewares;
+
+public class DefaultApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
 {
-    private readonly RequestDelegate _next;
-    private readonly IConfiguration _configuration;
+    private readonly RequestDelegate _next = next;
+    private readonly IConfiguration _configuration = configuration;
     private const string APIKEY_HEADER_NAME = "X-API-KEY";
-    private readonly string _apiKey = String.Empty;
 
-    public DefaultApiKeyMiddleware(RequestDelegate next, IConfiguration configuration)
+    public async Task InvokeAsync(HttpContext context)
     {
-        _next = next;
-        _configuration = configuration;
-        _apiKey = _configuration["ApiKeys:Default"]!;
+        var defaultApiKey = _configuration["ApiKeys:Default"] ?? null;
+
+        if (string.IsNullOrEmpty(defaultApiKey) || !context.Request.Headers.TryGetValue(APIKEY_HEADER_NAME, out var providedApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid API key or API key is missing.");
+            return;
+        }
+
+        if(!string.Equals(providedApiKey, defaultApiKey))
+        {
+            context.Response.StatusCode = 401;
+            await context.Response.WriteAsync("Invalid API key.");
+            return;
+        }
+
+        await _next(context);
     }
 }
